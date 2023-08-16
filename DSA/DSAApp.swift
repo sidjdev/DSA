3e79b8accc64a3a401f2823021a744117981d334
Sidharth J Dev <sid.j.dev@gmail.com>
2023-08-16 10:22:16 +0530

    Initial Commit

---
+++ DSA/DSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSAApp.swift
+//  DSA
+//
+//  Created by Sidharth J Dev on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
